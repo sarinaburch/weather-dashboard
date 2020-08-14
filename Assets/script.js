4ef14ff3738bf9005a04230eca3272772a5630a0
Sarina Burch <sarinaburch52@gmail.com>
2020-08-14 19:35:25 -0400

    added API, updated HTML IDs, created function to show current city weather with icon after being searched

--- Assets/script.js
+++ Assets/script.js
@@ -1,17 +1,37 @@
-$(document).ready(function() {
 
-// creating global variables
 
-var searchBox = ("search-bar");
-var searchButton = ("searchButton")
-var cityWeather = ("city-weather");
-var fiveDay = ("five-day");
+    // creating global variables
+    var cityWeather = ("city-weather");
+    var fiveDay = ("five-day");
+   
 
-// creating funtionality to search button and adding to local storage and adding API Key
+    // creating funtionality to search button and adding to local storage and adding API Key
 
-var cityInput = $("#search-bar")
-$(".searchButton").click(function(){
-    localStorage.setItem("searchButton", "city");
-});
-var searchedCities = localStorage.getItem("searchButton");
-});
+    $("#search-button").on("click", function(event) {
+        event.preventDefault();
+
+        
+        var city = $("#search-bar").val();
+        var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + ",us&APPID=97dffc6fa8edcd6f709b8c102d9878cf";
+
+        $.ajax( {
+            url: queryURL,
+            method: "GET"
+        }).then(function(response) {
+           
+            var tempC = response.main.temp;
+            var tempF = (tempC - 273.15) * 1.80 + 32;
+            var icon = "https://api.openweathermap.org/img/w/" + response.weather[0].icon + ".png";
+            var iconImg = $("<img>");
+
+            $("#temperature").text("Temperature: " + tempF.toFixed(2) +"\xB0" +"F"); 
+            $("#humidity").text("Humidty: " + response.main.humidity + "%");
+            $("#windSpeed").text("Wind Speed: " + response.wind.speed + "MPH");
+           
+            $("#city-weather").text(response.name);
+            iconImg.attr("src", icon)
+            $("#city-weather").append(iconImg);
+            
+        });
+    });
+
